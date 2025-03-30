787ca956bec0624ac2c1539ed5dba17c3a16e802
Will Wilson <will_wilson@hotmail.com>
2025-03-30 14:12:22 +0100

    Blend navigation bar, add icon - v1.0.1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import changeNavigationBarColor from "react-native-navigation-bar-color";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
